40b49ee9cd21a4cf2df1fce75ed851459aa32977
Zhouzhifeng <1121238923@qq.com>
2019-12-10 11:29:33 +0800

    自定义文件下载，文件名重命名

--- SpiderObject/SpiderObject/pipelines.py
+++ SpiderObject/SpiderObject/pipelines.py
@@ -4,8 +4,13 @@
 #
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://docs.scrapy.org/en/latest/topics/item-pipeline.html
+import datetime
+import os
 
+import scrapy
 from scrapy.exceptions import DropItem
+from scrapy.pipelines.files import FilesPipeline
+
 
 class TextPipeline(object):
     def __init__(self):
@@ -17,3 +22,13 @@ class TextPipeline(object):
             return item
         else:
             return DropItem('Missing Text')
+
+class MyfilesPipeline(FilesPipeline):
+    def get_media_requests(self, item, info):
+        for url in item["file_urls"]:
+            yield scrapy.Request(url)
+
+    def file_path(self, request, response=None, info=None):
+        today = datetime.date.today()
+        path = '/qhjg/' + today.strftime('%Y%m%d') + '/' + today.strftime('%Y%m%d') + '_FutureDataDelsettle.xls'
+        return path
--- SpiderObject/SpiderObject/settings.py
+++ SpiderObject/SpiderObject/settings.py
@@ -67,9 +67,9 @@ ROBOTSTXT_OBEY = True
 ITEM_PIPELINES = {
    #'SpiderObject.pipelines.SpiderobjectPipeline': 300,
     'SpiderObject.pipelines.TextPipeline':300,
-    'scrapy.pipelines.files.FilesPipeline': 1,
+    'SpiderObject.pipelines.MyfilesPipeline':1,
 }
-FILES_STORE = './scrapy'
+FILES_STORE = './scrapyFile'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
