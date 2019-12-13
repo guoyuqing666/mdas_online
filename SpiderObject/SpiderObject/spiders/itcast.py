b9ccfb6d51ba8ad10322248779fe096908f48458
Zhouzhifeng <1121238923@qq.com>
2019-12-13 14:20:36 +0800

    郑州商品交易所增加结算参数爬取

--- SpiderObject/SpiderObject/spiders/itcast.py
+++ SpiderObject/SpiderObject/spiders/itcast.py
@@ -8,7 +8,7 @@ from ..items import ExamplesItem
 class ItcastSpider(scrapy.Spider):
     name = 'itcast'
     allowed_domains = ['itcast.cn']
-    start_urls = ['http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/20191104/FutureDataDelsettle.htm']
+    start_urls = ['http://www.czce.com.cn/cn/jysj/jscs/H770303index_1.htm']
     custom_settings = {
         'ITEM_PIPELINES': {'SpiderObject.pipelines.MyfilesPipeline': 300,}
     }
@@ -18,7 +18,9 @@ class ItcastSpider(scrapy.Spider):
         # oneday = datetime.timedelta(days=1)
         # yesterday = today - oneday
         # download_url = 'http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/' + today.strftime('%Y%m%d') + '/FutureDataDelsettle.xls'  # 解析得到要下载的url
-        download_url = 'http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/20191210/FutureDataDelsettle.xls'  # 解析得到要下载的url
-        item = ExamplesItem()
-        item['file_urls'] = [download_url]
-        yield item
+        urls = ['FutureDataDelsettle.xls', 'FutureDataClearParams.xls']
+        for url in urls:
+            download_url = 'http://www.czce.com.cn/cn/DFSStaticFiles/Future/2019/20191210/' + url  # 解析得到要下载的url
+            item = ExamplesItem()
+            item['file_urls'] = [download_url]
+            yield item
