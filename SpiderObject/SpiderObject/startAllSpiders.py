91910ed1bcb16f51b903739fb4a6ac45221f7505
gyq <532230711@qq.com>
2019-12-13 15:04:32 +0800

    gyq_gui_start

---
+++ SpiderObject/SpiderObject/startAllSpiders.py
@@ -0,0 +1,39 @@
+from scrapy import cmdline
+
+# cmdline.execute("scrapy crawlall".split())
+# cmdline.execute("scrapy crawl dzh -o dzh.csv".split())
+
+from tkinter import *
+import time
+
+from tkinter import *
+
+
+def callback():
+    var.set('程序正在运行，请稍等...')
+    cmdline.execute("scrapy crawlall".split())
+    cmdline.execute("scrapy crawl dzh -o dzh.csv".split())
+
+
+root = Tk()
+
+frame1 = Frame(root)  # Frame    框架控件；在屏幕上显示一个矩形区域，多用来作为容器
+frame2 = Frame(root)
+
+todayDate = time.strftime('%Y-%m-%d', time.localtime(time.time()))
+var = StringVar()  # 设置字符串
+var.set("今天的日期是:"+todayDate+"\n 要开始并行运行所有爬虫吗？")
+
+textLabel = Label(frame1,
+                  textvariable=var,
+                  justify=LEFT)
+
+textLabel.pack(side=LEFT)
+
+theButton = Button(frame2, text="爬取全部内容", command=callback)  # 定义一个按钮
+theButton.pack()
+
+frame1.pack(padx=10, pady=10)  # 定义位置
+frame2.pack(padx=50, pady=50)
+
+mainloop()
